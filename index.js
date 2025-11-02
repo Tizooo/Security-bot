0fb8afc18d5e97777c0f553f4c0aacc0070036cf
Tizo <34001316+Tizooo@users.noreply.github.com>
2025-11-02 13:03:45 +0100

    Fix formatting of GatewayIntentBits.MessageContent

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ const client = new Client({
     GatewayIntentBits.Guilds,
     GatewayIntentBits.GuildMessages,
     GatewayIntentBits.GuildMembers,
-    GatewayIntentBits.MessageContent, // needed to detect new messages
+    GatewayIntentBits.MessageContent,
     GatewayIntentBits.DirectMessages,
   ],
   partials: [Partials.Channel],
